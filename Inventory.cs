f1f820b81055ab391d7db12b6bf1b424fdb53875
amione <57235791+xamionex@users.noreply.github.com>
2024-12-24 16:56:55 +0100

    null check in InventoryBackgrounds

--- Inventory.cs
+++ Inventory.cs
@@ -10,132 +10,135 @@ internal class InventoryPatch
 {
   [HarmonyPatch(typeof(Inventory), nameof(Inventory.show))]
   [HarmonyPostfix]
-  public static void InventoryBackgrounds(Inventory __instance, string labelName = "")
+  public static void InventoryBackgrounds(Inventory instance, string labelName = "")
   {
-    GameObject gameObject = __instance.thisUI;
-    PositionMe positionMe = gameObject.GetComponent<PositionMe>();
+    var gameObject = instance.thisUI;
+    if (!gameObject.TryGetComponent<PositionMe>(out var positionMe)) return;
     if (Plugin.WorkbenchInventoryModification.Value && labelName == "Storage")
     {
-      positionMe.offset = new Vector2(__instance.position.x + Plugin.StorageXOffset.Value, __instance.position.z + Plugin.StorageZOffset.Value);
+      positionMe.offset = new Vector2(instance.position.x + Plugin.StorageXOffset.Value, instance.position.z + Plugin.StorageZOffset.Value);
     }
-    if (Plugin.CraftingModification.Value && __instance.invType == Inventory.InvType.crafting)
+
+    if (!Plugin.CraftingModification.Value || instance.invType != Inventory.InvType.crafting) return;
+    positionMe.offset = new Vector2(instance.position.x + Plugin.CraftingXOffset.Value, instance.position.z + Plugin.CraftingZOffset.Value);
+    UpgradeItemMenuPatch.CraftingPos.x = positionMe.offset.x;
+    UpgradeItemMenuPatch.CraftingPos.y = positionMe.offset.y;
+    if (!instance.isWorkbench) return;
+    foreach (var child in gameObject.GetComponentsInChildren<Transform>())
     {
-      positionMe.offset = new Vector2(__instance.position.x + Plugin.CraftingXOffset.Value, __instance.position.z + Plugin.CraftingZOffset.Value);
-      UpgradeItemMenuPatch.craftingPos.x = positionMe.offset.x;
-      UpgradeItemMenuPatch.craftingPos.y = positionMe.offset.y;
-      if (__instance.isWorkbench)
-      {
-        foreach (var child in gameObject.GetComponentsInChildren<Transform>())
-        {
-          if (child.name == "WorkbenchBackground")
-          {
-            UnityEngine.Object.Destroy(child.gameObject);
-            float extraSlots = Plugin.CraftingRightSlots.Value - 5f;
-            float xPosition = 119f + 31f * extraSlots;
-            float xScale = 1f + 0.21f * extraSlots;
-            GameObject workbenchBackground = Core.AddPrefab("UI/WorkbenchBackground", new Vector3(xPosition, -10f, -233f), Quaternion.Euler(90f, 0f, 0f), gameObject.gameObject, false);
-            workbenchBackground.transform.localScale = new Vector3(xScale, 1f, 1f);
-            Singleton<InventoryController>.Instance.repairBtn.transform.position = gameObject.transform.position + new Vector3(xPosition, 5f, -409f);
-            Singleton<InventoryController>.Instance.upgradeBtn.transform.position = gameObject.transform.position + new Vector3(xPosition, 5f, -451f);
-            Singleton<InventoryController>.Instance.upgradeWorkbenchBtn.transform.position = gameObject.transform.position + new Vector3(xPosition, 5f, -503f);
-            break;
-          }
-        }
-      }
+      if (child.name != "WorkbenchBackground") continue;
+      UnityEngine.Object.Destroy(child.gameObject);
+      var extraSlots = Plugin.CraftingRightSlots.Value - 5f;
+      var xPosition = 119f + 31f * extraSlots;
+      var xScale = 1f + 0.21f * extraSlots;
+      var workbenchBackground = Core.AddPrefab("UI/WorkbenchBackground", new Vector3(xPosition, -10f, -233f), Quaternion.Euler(90f, 0f, 0f), gameObject.gameObject, false);
+      workbenchBackground.transform.localScale = new Vector3(xScale, 1f, 1f);
+      Singleton<InventoryController>.Instance.repairBtn.transform.position = gameObject.transform.position + new Vector3(xPosition, 5f, -409f);
+      Singleton<InventoryController>.Instance.upgradeBtn.transform.position = gameObject.transform.position + new Vector3(xPosition, 5f, -451f);
+      Singleton<InventoryController>.Instance.upgradeWorkbenchBtn.transform.position = gameObject.transform.position + new Vector3(xPosition, 5f, -503f);
+      break;
     }
   }
 
   [HarmonyPatch(typeof(Inventory), nameof(Inventory.show))]
   [HarmonyPrefix]
-  public static void InventorySlots(Inventory __instance, string labelName = "")
+  public static void InventorySlots(Inventory instance, string labelName = "")
   {
-    int maxSlots = 0;
+    var maxSlots = 0;
 
     if (labelName == "Storage")
     {
       if (Plugin.WorkbenchInventoryModification.Value)
       {
-        __instance.maxColumns = Plugin.RightSlots.Value;
-        maxSlots = __instance.maxColumns * Plugin.DownSlots.Value;
+        instance.maxColumns = Plugin.RightSlots.Value;
+        maxSlots = instance.maxColumns * Plugin.DownSlots.Value;
       }
       else
       {
-        __instance.maxColumns = 6;
+        instance.maxColumns = 6;
         maxSlots = 48; // 6*8
       }
     }
     else
     {
-      switch (__instance.invType)
+      switch (instance.invType)
       {
         case Inventory.InvType.crafting:
           if (Plugin.CraftingModification.Value)
           {
-            __instance.maxColumns = Plugin.CraftingRightSlots.Value;
-            maxSlots = __instance.maxColumns * Plugin.CraftingDownSlots.Value;
+            instance.maxColumns = Plugin.CraftingRightSlots.Value;
+            maxSlots = instance.maxColumns * Plugin.CraftingDownSlots.Value;
           }
           else
           {
-            __instance.maxColumns = 5;
+            instance.maxColumns = 5;
             maxSlots = 35; // 5*7
           }
           break;
         case Inventory.InvType.hotbar:
           if (Plugin.HotbarSlots.Value)
           {
-            __instance.maxColumns = Plugin.HotbarRightSlots.Value;
-            maxSlots = __instance.maxColumns * Plugin.HotbarDownSlots.Value;
+            instance.maxColumns = Plugin.HotbarRightSlots.Value;
+            maxSlots = instance.maxColumns * Plugin.HotbarDownSlots.Value;
           }
           else
           {
-            __instance.maxColumns = 1;
+            instance.maxColumns = 1;
             maxSlots = 3 + Player.Instance.hotbarUpgrades;
           }
           break;
         case Inventory.InvType.playerInv:
           if (Plugin.InventorySlots.Value)
           {
-            __instance.maxColumns = Plugin.InventoryRightSlots.Value;
-            maxSlots = __instance.maxColumns * Plugin.InventoryDownSlots.Value;
+            instance.maxColumns = Plugin.InventoryRightSlots.Value;
+            maxSlots = instance.maxColumns * Plugin.InventoryDownSlots.Value;
           }
           else
           {
-            __instance.maxColumns = 2;
+            instance.maxColumns = 2;
             maxSlots = 12 + (2 * Player.Instance.inventoryUpgrades);
           }
           break;
         case Inventory.InvType.shop:
           if (Plugin.TraderSlots.Value)
           {
-            __instance.maxColumns = Plugin.TraderRightSlots.Value;
-            maxSlots = __instance.maxColumns * Plugin.TraderDownSlots.Value;
+            instance.maxColumns = Plugin.TraderRightSlots.Value;
+            maxSlots = instance.maxColumns * Plugin.TraderDownSlots.Value;
           }
           else
           {
-            __instance.maxColumns = 6;
+            instance.maxColumns = 6;
             maxSlots = 42; // 6*7
           }
           break;
+        case Inventory.InvType.itemInv:
+        case Inventory.InvType.shopExchangePlayer:
+        case Inventory.InvType.shopExchangeTrader:
+        case Inventory.InvType.leveling:
+        case Inventory.InvType.deathDrop:
+          break;
+        default:
+          throw new ArgumentOutOfRangeException();
       }
     }
     
-    if (maxSlots > 0 && maxSlots != __instance.slots.Count)
+    if (maxSlots > 0 && maxSlots != instance.slots.Count)
     {
       if (Plugin.LogDebug.Value)
       {
         Plugin.LogDivider();
         Plugin.Log.LogInfo($"Player has {Player.Instance.inventoryUpgrades} inventory upgrades and {Player.Instance.hotbarUpgrades} hotbar upgrades.");
       }
-      ChangeSlots(__instance, maxSlots);
+      ChangeSlots(instance, maxSlots);
     }
   }
-  public static void ChangeSlots(Inventory __instance, int MaximumSlots)
+  public static void ChangeSlots(Inventory instance, int maximumSlots)
   {
-    int difference = MaximumSlots - __instance.slots.Count;
+    var difference = maximumSlots - instance.slots.Count;
     if (Plugin.LogDebug.Value)
     {
       Plugin.LogDivider();
-      Plugin.Log.LogInfo($"Maximum slots allowed in \"{__instance.name}\" is {MaximumSlots} and we have {__instance.slots.Count}");
+      Plugin.Log.LogInfo($"Maximum slots allowed in \"{instance.name}\" is {maximumSlots} and we have {instance.slots.Count}");
 
       if (difference < 0)
       {
@@ -149,17 +152,17 @@ internal class InventoryPatch
     }
     while (difference < 0 && Plugin.RemoveExcess.Value)
     {
-      for (int i = MaximumSlots - 1; i < __instance.slots.Count; i++)
+      for (var i = maximumSlots - 1; i < instance.slots.Count; i++)
       {
-        __instance.slots.RemoveAt(i - 1);
+        instance.slots.RemoveAt(i - 1);
         difference++;
       }
     }
     while (difference > 0)
     {
-      for (int i = 0; i < difference; i++)
+      for (var i = 0; i < difference; i++)
       {
-        __instance.slots.Add(new InvSlot());
+        instance.slots.Add(new InvSlot());
         difference--;
       }
     }
@@ -168,9 +171,9 @@ internal class InventoryPatch
 
   [HarmonyPatch(typeof(Inventory), nameof(Inventory.initSlots))]
   [HarmonyPrefix]
-  static void InventoryItems(ref Inventory __instance)
+  static void InventoryItems(ref Inventory instance)
   {
-    string name = __instance.gameObject.name;
+    var name = instance.gameObject.name;
 
     // Check if the inventory name exists in CustomLoot
     if (!Plugin.CustomLoot.ContainsKey(name))
@@ -179,11 +182,11 @@ internal class InventoryPatch
       var itemsList = new JArray();
 
       // Loop through slots and add items dynamically
-      for (int i = 0; i < __instance.slots.Count; i++)
+      for (var i = 0; i < instance.slots.Count; i++)
       {
         var itemObject = new JObject
         {
-          { "item", __instance.slots[i].item?.type ?? "Empty" },
+          { "item", instance.slots[i].item?.type ?? "Empty" },
           { "minAmount", 1 },
           { "maxAmount", 1 },
           { "chance", 1f },
@@ -212,9 +215,9 @@ internal class InventoryPatch
 
     if (!(bool)lootEntry.Value["enabled"]) return;
 
-    for (int i = 0; i < __instance.slots.Count; i++)
+    for (var i = 0; i < instance.slots.Count; i++)
     {
-      if (!(bool)replaceSlot && __instance.slots[i] != null) continue;
+      if (!(bool)replaceSlot && instance.slots[i] != null) continue;
 
       // Get the item data for the current slot
       // lootItems can have less or more than this inventories slots
@@ -227,8 +230,8 @@ internal class InventoryPatch
       {
         continue;
       }
-      string itemName = itemData["item"].ToString();
-      float chance = (float)itemData["chance"];
+      var itemName = itemData["item"].ToString();
+      var chance = (float)itemData["chance"];
 
       if (itemName == "Empty") continue;
 
@@ -236,8 +239,8 @@ internal class InventoryPatch
       if (UnityEngine.Random.value <= chance)
       {
         // Add the item to the inventories slots
-        int amount = UnityEngine.Random.Range((int)itemData["minAmount"], (int)itemData["maxAmount"] + 1);
-        __instance.slots[i] = new() { item = ItemsDatabase.Instance.getItem(itemName, false), itemAmount = amount };
+        var amount = UnityEngine.Random.Range((int)itemData["minAmount"], (int)itemData["maxAmount"] + 1);
+        instance.slots[i] = new() { item = ItemsDatabase.Instance.getItem(itemName, false), itemAmount = amount };
       }
     }
   }
