3ad22fd941c46d8ca8f0819da039b9f5e3054a2e
Petar <57235791+xamionex@users.noreply.github.com>
2025-07-16 05:05:04 +0200

    Fix close button offset not working

--- DialogueWindow.cs
+++ DialogueWindow.cs
@@ -5,6 +5,9 @@ namespace DarkwoodCustomizer;
 
 internal class DialogueWindowPatch
 {
+  private static Vector3? _cachedCloseButtonOriginal;
+  private static Vector3? _cachedCloseButtonBackgroundOriginal;
+  
   [HarmonyPatch(typeof(DialogueWindow), nameof(DialogueWindow.openTrade))]
   [HarmonyPostfix]
   public static void InventoryBackgrounds(DialogueWindow __instance)
@@ -15,34 +18,48 @@ internal class DialogueWindowPatch
 
     var closeButton = __instance.inventoryBackground.Find("Close");
     var closeButtonBackground = __instance.inventoryBackground.Find("Background");
-    var acceptButton = __instance.exchangeTrader.transform.Find("AcceptTradeBtn");
-
-    var closeOffset = new Vector2(0, 0);
+    // Cache original positions if not already cached
+    if (!_cachedCloseButtonOriginal.HasValue)
+    {
+      _cachedCloseButtonOriginal = closeButton.position;
+      _cachedCloseButtonBackgroundOriginal = closeButtonBackground.position;
+    }
+    
     if (Plugin.TraderSlots.Value)
     {
       traderInventory.offset = new Vector2(traderInventory.offset.x + Plugin.TraderInventoryXOffset.Value, traderInventory.offset.y + Plugin.TraderInventoryZOffset.Value);
       exchangeTrader.offset = new Vector2(exchangeTrader.offset.x + Plugin.TraderBuyXOffset.Value, exchangeTrader.offset.y + Plugin.TraderBuyZOffset.Value);
       exchangePlayer.offset = new Vector2(exchangePlayer.offset.x + Plugin.TraderSellXOffset.Value, exchangePlayer.offset.y + Plugin.TraderSellZOffset.Value);
-      closeOffset = new Vector2(Plugin.TraderCloseXOffset.Value, Plugin.TraderCloseZOffset.Value);
     }
     
     var extraSlots = Plugin.InventoryRightSlots.Value - 2f;
+    var extraSlotsOffset = 0f;
     if (extraSlots > 0)
     {
       var inventoryBackground = __instance.inventoryBackground.GetComponent<PositionMe>();
       var inventoryBackgroundSprite = __instance.inventoryBackground.GetComponent<tk2dSprite>();
       inventoryBackgroundSprite.scale = new Vector3(2f + 0.65f * extraSlots, inventoryBackgroundSprite.scale.y, inventoryBackgroundSprite.scale.z);
-      inventoryBackground.offset.x += 31f * extraSlots;
-      traderInventory.offset.x += 31f * extraSlots;
-      exchangeTrader.offset.x += 31f * extraSlots;
-      exchangePlayer.offset.x += 31f * extraSlots;
+      extraSlotsOffset = 31f * extraSlots;
+      inventoryBackground.offset.x += extraSlotsOffset;
+      traderInventory.offset.x += extraSlotsOffset;
+      exchangeTrader.offset.x += extraSlotsOffset;
+      exchangePlayer.offset.x += extraSlotsOffset;
       inventoryBackground.init();
     }
     
     // we want to have the accept button and close button at the same spots, why is it separated like this, and why is AcceptTrade in "trader" ;-;
     // - 50 because it should be below accept and bg is -1 on y pos
-    closeButton.position = new Vector3(acceptButton.position.x + closeOffset.x, acceptButton.position.y, acceptButton.position.z - 50 + closeOffset.y);
-    closeButtonBackground.position = new Vector3(acceptButton.position.x + closeOffset.x, acceptButton.position.y - 1, acceptButton.position.z - 50 + closeOffset.y);
+    closeButton.position = new Vector3(
+      _cachedCloseButtonOriginal.Value.x + extraSlotsOffset + Plugin.TraderCloseXOffset.Value,
+      _cachedCloseButtonOriginal.Value.y,
+      _cachedCloseButtonOriginal.Value.z + Plugin.TraderCloseZOffset.Value
+    );
+        
+    closeButtonBackground.position = new Vector3(
+      _cachedCloseButtonBackgroundOriginal.Value.x + extraSlotsOffset + Plugin.TraderCloseXOffset.Value,
+      _cachedCloseButtonBackgroundOriginal.Value.y,
+      _cachedCloseButtonBackgroundOriginal.Value.z + Plugin.TraderCloseZOffset.Value
+    );
     
     traderInventory.init();
     exchangePlayer.init();
