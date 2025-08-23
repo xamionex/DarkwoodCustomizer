ef6be7cab28fc9fd49a54b3341d77177f892b6e5
Petar <57235791+xamionex@users.noreply.github.com>
2025-08-23 21:05:40 +0200

    Improve and add PostFX and vignette

--- CamMain.cs
+++ CamMain.cs
@@ -4,18 +4,56 @@ namespace DarkwoodCustomizer;
 
 internal class CamMainPatch
 {
-  [HarmonyPatch(typeof(CamMain), "Update")]
-  [HarmonyPostfix]
-  public static void CamUpdate(CamMain __instance)
-  {
-    if (Plugin.CameraModification.Value && Singleton<CamMain>.Instance.GetComponent<tk2dCamera>().ZoomFactor != Plugin.CameraFoV.Value)
+    private static bool _previousPostFxState;
+    private static bool _previousVignetteState;
+    private static float _previousFoV;
+    
+    // Cache child camera names to avoid repeated string operations
+    private static readonly string[] ChildCameraNames = 
     {
-      Singleton<CamMain>.Instance.GetComponent<tk2dCamera>().ZoomFactor = Plugin.CameraFoV.Value;
-      Singleton<CamMain>.Instance.transform.Find("CamLights").GetComponent<tk2dCamera>().ZoomFactor = Plugin.CameraFoV.Value;
-      Singleton<CamMain>.Instance.transform.Find("CamFOV").GetComponent<tk2dCamera>().ZoomFactor = Plugin.CameraFoV.Value;
-      Singleton<CamMain>.Instance.transform.Find("CamAmbient").GetComponent<tk2dCamera>().ZoomFactor = Plugin.CameraFoV.Value;
-      Singleton<CamMain>.Instance.transform.Find("CamLightning").GetComponent<tk2dCamera>().ZoomFactor = Plugin.CameraFoV.Value;
-      Singleton<CamMain>.Instance.transform.Find("CamTopMask").GetComponent<tk2dCamera>().ZoomFactor = Plugin.CameraFoV.Value;
+        "CamLights",
+        "CamFOV",
+        "CamAmbient",
+        "CamLightning",
+        "CamTopMask"
+    };
+    
+    [HarmonyPatch(typeof(CamMain), "Update")]
+    [HarmonyPostfix]
+    public static void CamUpdate(CamMain __instance)
+    {
+        if (!Plugin.CameraModification.Value) return;
+        
+        // Cache the main camera component
+        var mainCamera = __instance.GetComponent<tk2dCamera>();
+        
+        // Handle FoV changes
+        if (mainCamera.ZoomFactor != Plugin.CameraFoV.Value || _previousFoV != Plugin.CameraFoV.Value)
+        {
+            mainCamera.ZoomFactor = Plugin.CameraFoV.Value;
+            
+            // Update all child cameras
+            var instanceTransform = __instance.transform;
+            foreach (var cameraName in ChildCameraNames)
+            {
+                instanceTransform.Find(cameraName).GetComponent<tk2dCamera>().ZoomFactor = Plugin.CameraFoV.Value;
+            }
+            
+            _previousFoV = Plugin.CameraFoV.Value;
+        }
+        
+        // Cache the PostFX transform
+        var postFxTransform = __instance.transform.Find("CamPostFX");
+        
+        if (_previousPostFxState != Plugin.CameraDisablePostFX.Value)
+        {
+            postFxTransform.GetComponent<AmplifyColorEffect>().enabled = !Plugin.CameraDisablePostFX.Value;
+            _previousPostFxState = Plugin.CameraDisablePostFX.Value;
+        }
+        if (_previousVignetteState != Plugin.CameraDisableVignette.Value)
+        {
+            postFxTransform.GetComponent<Vignetting>().enabled = !Plugin.CameraDisableVignette.Value;
+            _previousVignetteState = Plugin.CameraDisableVignette.Value;
+        }
     }
-  }
 }
